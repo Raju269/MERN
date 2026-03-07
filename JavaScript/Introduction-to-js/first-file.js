11d739340514a247136180a218ea64b4d0385520
Rajukumar <rajuk631149@gmail.com>
2026-03-07 13:48:47 +0530

    basic introduction and code part done

---
+++ JavaScript/Introduction-to-js/first-file.js
@@ -0,0 +1,69 @@
+console.log("hello Coder Army");
+
+// control + `  it will open your terminal
+// cd "C:\Users\Anuj\Desktop\NEXUS\NEXUS\JavaScript\Day01 (Introduction, History, Declaring Variables)"
+// node first.js  to run the file
+
+// let const var
+// var old tarike, don't use it.
+
+let num = 10.5;
+console.log(num);
+
+let ar = "Rohit";
+ar = "mohit";
+console.log(ar)
+
+const ids = 20;
+console.log(ids);
+// ids = 30; values can't be changed
+
+var x = 10;
+x=20;
+console.log(x);
+
+// in other languages, array store data in contiguous memory location, in JS it is not like that
+// JS need : calculate, change dynamically when we click a button
+// can manipulate html too
+
+// The first web browser ever built was WorldWideWeb, created in 1990 by Tim Berners-Lee at CERN.
+// Later it was renamed Nexus to avoid confusion with the World Wide Web itself.
+
+//Netscape was one of the most important early web browsers and a key player in early internet history.
+//JavaScript was created inside Netscape in 1995 by Brendan Eich, during the race to make web pages interactive.
+
+
+// Microsoft owns:
+// • Internet Explorer
+// • Microsoft Edge
+
+// Google owns:
+// • Google Chrome
+
+// Apple owns:
+// • Safari
+
+// Mozilla Foundation owns:
+// • Firefox
+
+// Tim Berners-Lee / CERN (historically)
+// • WorldWideWeb → Nexus (first browser)
+
+// Netscape Communications (defunct)
+// • Netscape Navigator
+// (Netscape was later bought by AOL.)
+
+// ECMA : to standardize JS
+// ECMAScript is the standardized specification that JavaScript is based on. 
+// It defines the core features and syntax of the language, ensuring consistency across different implementations.
+
+// each browser has its own JS engine
+// V8 - Google Chrome, Microsoft Edge
+// SpiderMonkey - Mozilla Firefox
+// JavaScriptCore (Nitro) - Apple Safari
+// V8 made in c++
+
+//jit is just in time compiler
+// It improves performance by compiling code on the fly, rather than ahead of time.
+
+// Node.js is an open-source JavaScript runtime that allows you to run JS outside the browser—onservers, desktops, and more. It uses Chromeʼs V8 engine for execution andprovides additional APIs to interact with files, networking, and OS resource management.
